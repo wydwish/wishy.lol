d68e912d9b639c4fcb56828143ada7e368255aa0
wydwish <162378107+wydwish@users.noreply.github.com>
2024-04-03 16:16:46 -0500

    Create bypass.lua

---
+++ miscellaneous/bypass.lua
@@ -0,0 +1,62 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local tablefind = table.find
+local MainEvent = ReplicatedStorage.MainEvent
+local SpoofTable = {
+	WalkSpeed = 16,
+	JumpPower = 50
+}
+
+local Flags = {
+    "TeleportDetect",
+    "OneMoreTime",
+    "CHECKER_1",
+    "Kick",
+    "BreathingHAMON",
+    "BR_KICKPC",
+    "KICKREMOTE",
+    "CHECKER",
+    "PERMAIDBAN",
+    "checkingSPEED",
+    "GUI_CHECK",
+    "BANREMOTE",
+}
+
+local __namecall
+__namecall = hookmetamethod(game, "__namecall", function(...)
+	local args = {...}
+	local self = args[1]
+	local method = getnamecallmethod()
+	local caller = getcallingscript()
+
+	if (method == "FireServer" and self == MainEvent and tablefind(Flags, args[2])) then
+		return
+	end
+
+	if (not checkcaller() and getfenv(2).crash) then
+		hookfunction(getfenv(2).crash, function()
+			warn("nuh uh") 
+		end)
+	end
+
+	return __namecall(...)
+end)
+
+local __index
+__index = hookmetamethod(game, "__index", function(t, k)
+	if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
+		return SpoofTable[k]
+	end
+
+	return __index(t, k)
+end)
+
+local __newindex
+__newindex = hookmetamethod(game, "__newindex", function(t, k, v)
+	if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
+		SpoofTable[k] = v
+		return
+	end
+
+	return __newindex(t, k, v)
+end)
